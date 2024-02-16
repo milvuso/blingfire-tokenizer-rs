f54609257090ce870fabddb68b2f60d73ae1542a
milvuso <112140988+milvuso@users.noreply.github.com>
2024-02-16 16:18:43 +0000

    making it work

--- blingfire/src/lib.rs
+++ blingfire/src/lib.rs
@@ -195,3 +195,7 @@ mod tests {
     }
 
     #[test]
+    fn text_to_sentences_string_smaller_than_output() {
+        let mut parsed = "hello".to_owned();
+        text_to_sentences(TEST_TEXT, &mut parsed).unwrap();
+        assert_eq!(TEST_TEXT_SENTENCES, parsed.as_str());
