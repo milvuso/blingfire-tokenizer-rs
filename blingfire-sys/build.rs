8f3b16f286196922b7d39f12602da22846ada544
milvuso <112140988+milvuso@users.noreply.github.com>
2024-03-14 12:49:29 +0000

    Add redux

--- blingfire-sys/build.rs
+++ blingfire-sys/build.rs
@@ -4,3 +4,7 @@ use cmake::Config;
 fn main() {
     let destination = Config::new("BlingFire")
         .always_configure(true)
+        .define("BLING_FIRE_VERSION_MAJOR", "1")
+        .define("BLING_FIRE_VERSION_MINOR", "0")
+        .build_target("blingfiretokdll_static")
+        .build();
